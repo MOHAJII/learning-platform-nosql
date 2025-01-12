db3ec1a95d284b6560f70015b3a524c897b15ce5
Pradeep Antil <hmohammediaji@gmail.com>
2025-01-12 21:59:55 +0100

    add redis configuration

--- src/services/mongoService.js
+++ src/services/mongoService.js
@@ -1,11 +1,41 @@
 const { ObjectId } = require("mongodb");
+const db = require("../config/db");
 
-// Fonctions utilitaires pour MongoDB
-async function findOneById(collection, id) {
-  // TODO: Implémenter une fonction générique de recherche par ID
+async function findOneById(collectionName, id) {
+  try {
+    const database = db.getDb();
+    return await database.collection(collectionName).findOne({
+      _id: new ObjectId(id),
+    });
+  } catch (error) {
+    console.error("MongoDB find error: ", error);
+    throw error;
+  }
+}
+
+async function insertOne(collectionName, data) {
+  try {
+    const database = db.getDb();
+    return await database.collection(collectionName).insertOne(data);
+  } catch (error) {
+    console.error("MongdoDB insert error: ", error);
+    throw error;
+  }
+}
+
+async function findMany(collectionName, query = {}, options = {}) {
+  try {
+    const database = db.getDb();
+    return await database.collection(collectionName).find(query, options).toArray();
+  } catch (error) {
+    console.error("MongoDB find error: ", error);
+    throw error;
+  }
 }
 
 // Export des services
 module.exports = {
-  // TODO: Exporter les fonctions utilitaires
+  findOneById, 
+  insertOne, 
+  findMany
 };
--- src/services/redisService.js
+++ src/services/redisService.js
@@ -1,8 +1,33 @@
-// Fonctions utilitaires pour Redis
-async function cacheData(key, data, ttl) {
-  // TODO: Implémenter une fonction générique de cache
+const db = require('../config/db')
+
+async function cacheData(key, data, ttl = 3600) {
+  try {
+    const redisClient = db.getRedisClient();
+    await redisClient.setEx(
+      key, 
+      ttl, 
+      typeof data == 'string' ? data : JSON.stringify(data)
+    );
+  } catch (error) {
+    console.error("Redis cache error ", error);
+    throw error;
+    
+  }
+}
+
+async function getCacheData(key) {
+  try {
+    const redisClient = db.getRedisClient();
+    const data = redisClient.get(key);
+    return data ? JSON.parse(data) : null
+  } catch (error) {
+    console.error('Redis get error:', error);
+    throw error;
+  }
+  
 }
 
 module.exports = {
-  // TODO: Exporter les fonctions utilitaires
+  cacheData,
+  getCacheData
 };
